878d4c8f9c73e89ba58c5eb1d8f03b3c8fc6a06f
Alex <alexandra.lafleur@hotmail.de>
2015-06-09 10:42:06 +0200

    commit

---
+++ RestTest/src/xml/test/Hello.java
@@ -0,0 +1,28 @@
+package xml.test;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/hello")
+public class Hello {
+
+  // This method is called if XML is request
+  @GET
+  @Path("question")
+  public String sayXMLHello() {
+    return "<?xml version=\"1.0\"?>" + "<block><question> What is this language? </question>"
+    		+ "<sentence>Bla Bla Blu</sentence> <translation>Bli Bli Ble</translation></block>";
+  }
+
+  // This method is called if HTML is request
+  @GET
+  @Produces(MediaType.TEXT_XML)
+  @Path("welcome")
+  public String sayWelcomeGeek() {
+    return "<html> " + "<title>" + "Hello Jersey" + "</title>"
+        + "<body><h1>" + "Hello Geek" + "</body></h1>" + "</html> ";
+  }
+
+} 
